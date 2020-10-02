066c64206b6ce28e0f2039a81f12326f841c85e4
gundurraga <67235552+gundurraga@users.noreply.github.com>
2020-10-01 20:30:00 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("night-mode");
- }
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  document.body.classList.add("night-mode");
+} else {
+  document.body.classList.remove("night-mode");
+}
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "night") {
+  document.body.classList.add("night-mode");
+} else {
+  document.body.classList.remove("night-mode");
+}
+
+//This save the user preference of theme on local storage, in the future should be saved by cookies
+const btnNightMode = document.querySelector(".btn-nightMode");
+btnNightMode.addEventListener("click", function () {
+  document.body.classList.toggle("night-mode");
+
+  let theme = "light";
+  if (document.body.classList.contains("night-mode")) {
+    theme = "night";
+  }
+  localStorage.setItem("theme", theme);
+});
